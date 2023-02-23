5c81977027c77d11ffcf06bfe839df47cf6e8e71
Agustin Ahumada <agustin.ahumada@dirmod.com>
2023-02-23 20:31:52 +0000

    Updated c103-02-promtheus-outputs.tf

--- c103-02-promtheus-outputs.tf
+++ c103-02-promtheus-outputs.tf
@@ -1,5 +1,7 @@
+/*
 # Helm Release Outputs
 output "prometheus_helm_metadata" {
   description = "Metadata Block outlining status of the deployed release."
   value = helm_release.prometheus.metadata
 }
+*/
