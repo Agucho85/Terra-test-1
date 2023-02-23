e1d89d418488ff56fae091b0d14e23a5438a58d2
Agustin Ahumada <agustin.ahumada@dirmod.com>
2023-02-23 20:44:11 +0000

    Updated c100-02-metrics-server-install.tf

--- c100-02-metrics-server-install.tf
+++ c100-02-metrics-server-install.tf
@@ -10,6 +10,6 @@ resource "helm_release" "metrics_server_release" {
     aws_eks_cluster.eks_cluster,
     helm_release.ebs_csi_driver,
     kubernetes_namespace_v1.namespace,
-    # helm_release.prometheus
+    helm_release.prometheus
   ]
 }
