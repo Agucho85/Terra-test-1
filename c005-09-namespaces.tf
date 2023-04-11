548c303dcc7b9fbd84460a8b1ccba61b07a54c78
agustin.manessi <agustin.manessi@dirmod.com>
2023-04-11 17:51:40 +0200

    add istio delete

--- c005-09-namespaces.tf
+++ c005-09-namespaces.tf
@@ -1,14 +1,4 @@
 # Resource: k8s namespace
-/*
-resource "kubernetes_namespace_v1" "k8s_dev" {
-  metadata {
-    name = "dev"
-    labels = {
-      istio-injection = "enabled"
-    }
-  }
-}
-*/
 resource "kubernetes_namespace_v1" "namespace" {
   for_each = var.namespace
   metadata {
--- c104-03-istio-outputs.tf
+++
@@ -1,5 +0,0 @@
-# # Helm Release Outputs
-# output "istio_helm_metadata" {
-#   description = "Metadata Block outlining status of the deployed release."
-#   value = helm_release.istio.metadata
-# }
